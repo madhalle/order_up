8d17cf81be8697939e921a69ff443a810657b84d
Mad Hallé <mad.halle@icloud.com>
2020-05-29 09:52:36 -0600

    User story 1 complete

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "when visiting the dishes show page" do
+  before :each do
+    @chef1 = Chef.create!(name: "Madeleine")
+    @dish1 = @chef1.dishes.create!(name: "Ramen Noodles", description: "Delicious, absolutely delicious")
+    @noodles = @dish1.ingredients.create!(name: "noodles", calories: 200)
+    @egg = @dish1.ingredients.create!(name: "soft boiled egg", calories: 20)
+    @broth = @dish1.ingredients.create!(name: "veggie broth", calories: 20)
+    @vegetables = @dish1.ingredients.create!(name: "veggies", calories: 40)
+  end
   it "will see list of ingredients for dish and chefs name" do
+    visit "/dishes/#{@dish1.id}"
 
+    expect(page).to have_content(@chef1.name)
+    expect(page).to have_content(@noodles.name)
+    expect(page).to have_content(@egg.name)
+    expect(page).to have_content(@broth.name)
+    expect(page).to have_content(@vegetables.name)
   end
 
 end
 # Order up is an app that tracks chefs, the dishes that they make, and the ingredients that are in those dishes. Chef's have many dishes. Dishes belong to a chef. Dishes have many Ingredients and Ingredients have many dishes. Ingredients should have name and calories.
-#
-# Two migrations have been made for you. The one to many relationship has been set up. You will need to create migrations for the many to many relationship between ingredients and dishes. You do not need to do model testing for validations, but you do need to test any relationships and model methods that you create.
-#
-# (Note: The plural of dish is dishes. The plural of chef is chefs.)
-#
-# Instructions
-# * Work on this challenge independently.
-# * Complete the stories below
-# * Commit Frequently
-# * Push your code to your fork once the time is up (not before!)
+
 #
 # Story 1 of 3
 # As a visitor
