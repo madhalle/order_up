df9a4534e60beb3f47cdd9339a1f8eff01882b94
Mad Hallé <mad.halle@icloud.com>
2020-05-29 11:03:30 -0600

    attempted extension

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,13 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  # def top_picks
+  #
+  #   dishes.each do |dish|
+  #     dish.ingredients.max(3) do |ingredient|
+  #       ingredient.dishes.count
+  #     end
+  #   end
+  #   # require "pry"; binding.pry
+  # end
 end
--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -3,14 +3,20 @@ RSpec.describe "when visiting chefs show page" do
   before :each do
     @chef1 = Chef.create!(name: "Madeleine")
     @chef2 = Chef.create!(name: "Bri")
+
     @dish1 = @chef1.dishes.create!(name: "Ramen Noodles", description: "Delicious, absolutely delicious")
+    @dish3 = @chef1.dishes.create!(name: "Good Pasta", description: "Delicious, really absolutely delicious")
+
     @dish2 = @chef2.dishes.create!(name: "Scrambled eggs", description: "Delicious, absolutely delicious")
+
     @noodles = Ingredient.create!(name: "noodles", calories: 200)
     @egg = Ingredient.create!(name: "soft boiled egg", calories: 20)
     @egg = Ingredient.create!(name: "soft boiled egg", calories: 20)
     @milk = Ingredient.create!(name: "Milk", calories: 60)
     @broth = Ingredient.create!(name: "veggie broth", calories: 20)
     @vegetables = Ingredient.create!(name: "veggies", calories: 40)
+    @cheese = Ingredient.create!(name: "parmesan", calories: 50)
+
     # @noodles = @dish1.ingredients.create!(name: "noodles", calories: 200)
     # @egg = @dish1.ingredients.create!(name: "soft boiled egg", calories: 20)
     # @egg = @dish2.ingredients.create!(name: "soft boiled egg", calories: 20)
@@ -22,7 +28,12 @@ RSpec.describe "when visiting chefs show page" do
     DishesIngredient.create!(ingredient: @egg, dish: @dish1 )
     DishesIngredient.create!(ingredient: @broth, dish: @dish1 )
     DishesIngredient.create!(ingredient: @vegetables, dish: @dish1 )
+
     DishesIngredient.create!(ingredient: @milk, dish: @dish2 )
+
+    DishesIngredient.create!(ingredient: @milk, dish: @dish3 )
+    DishesIngredient.create!(ingredient: @egg, dish: @dish3 )
+    DishesIngredient.create!(ingredient: @noodles, dish: @dish3 )
   end
 
   it "can see name of chef and link to chefs ingredient index" do
@@ -36,15 +47,20 @@ RSpec.describe "when visiting chefs show page" do
     expect(page).to have_content(@egg.name)
     expect(page).to have_content(@broth.name)
     expect(page).to have_content(@vegetables.name)
-    expect(page).to_not have_content(@milk.name)
+    # expect(page).to_not have_content(@milk.name)
+  end
+
+  xit "can see name of chef and 3 most popular chefs ingredients" do
+    visit "/chefs/#{@chef1.id}"
+
+    expect(page).to have_content(@egg.name)
+    expect(page).to have_content(@noodles.name)
   end
 
 end
-# Story 3 of 3
+
 # As a visitor
 # When I visit a chef's show page
-# I see the name of that chef
-# And I see a link to view a list of all ingredients that this chef uses in their dishes
-# When I click on that link
-# I'm taken to a chef's ingredient index page
-# and I can see a unique list of names of all the ingredients that this chef uses
+# I see the three most popular ingredients that the chef uses in their dishes
+# (Popularity is based off of how many dishes use that ingredient)
+# Collapse
